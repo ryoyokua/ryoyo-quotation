180ed81153ec23350809ba83670fc37d131d6717
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 15:51:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -26,3 +26,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v105 - separate construction area and sealing UI
 // app revision v106 - result separation, sealing flow, spinner consistency, tank UI
 // app revision v107 - terminology, realtime calculations, unentered sealing, spinner fix
+// app revision v108 - unified mobile deduction cards
