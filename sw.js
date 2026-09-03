575b67cfa7a7946a54fb0d66b2a46ac9421390a0
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 14:49:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,3 +12,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v91 - common sealing calculation and raw-area display cleanup
 // app revision v92 - seal field alignment
 // app revision v93 - quantity UI unified
+// app revision v94 - desktop two-column quantity screens
