bd2c0be4e1978181f2e71f8b3589a3c592722d19
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 15:00:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,3 +14,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v93 - quantity UI unified
 // app revision v94 - desktop two-column quantity screens
 // app revision v95 - sealing width/depth spinner step 1
+// app revision v96 - roof/flat/vessel stacked vertical layout
