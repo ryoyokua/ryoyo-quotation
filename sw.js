9125359bdc5681fada29a51d935dc219607d0fbe
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 15:34:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -23,3 +23,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v102 - mobile quantity heading/button layout fix
 // app revision v103 - desktop quantity add buttons right aligned
 // app revision v104 - rebuilt mobile quantity input cards
+// app revision v105 - separate construction area and sealing UI
