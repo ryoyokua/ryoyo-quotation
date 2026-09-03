8cd8651f2a40f313a39bf8cb8400557791388618
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 16:01:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -29,3 +29,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v108 - unified mobile deduction cards
 // app revision v109 - integer spinner normalization
 // app revision v110 - tank deduction mobile layout fix
+// app revision v111 - explicit tank deduction mobile card layout
