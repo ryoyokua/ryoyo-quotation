8fbcde3f30f8f48530cb4cdb63cdd0908c9cbd96
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-03 14:32:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,3 +10,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v89 - Sheets project list is source of truth
 // app revision v90 - deductions and joints
 // app revision v91 - common sealing calculation and raw-area display cleanup
+// app revision v92 - seal field alignment
