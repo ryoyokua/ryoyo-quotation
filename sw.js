68d163417ceb72b34ba612106c4d7425f9e0d663
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-04 11:32:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -42,4 +42,5 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v123 - default area rounding: ceil to 0.1 square meter
 // app revision v124 - merge roof wave material choices: folded plate and other
 // app revision v125 - lower roof deduction and sealing add buttons slightly
-// app revision v126 - normalize tank deduction area display
+// app revision v126 - neutral mobile deduction area display
+// app revision v127 - unify mobile deduction area result boxes in green, full width and same shape
