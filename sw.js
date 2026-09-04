da4d3b79856932096ade364c06bc75cb1f35c300
菱洋(株)赤木 <ku-akagi@ryo-yo.co.jp>
2026-09-04 12:04:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -46,3 +46,4 @@ self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()))
 // app revision v127 - unify mobile deduction area result boxes in green, full width and same shape
 // app revision v128 - tank deduction result width aligned with other deduction cards
 // app revision v129 - compact collapsible sealing material calculator
+// app revision v130 - sync sealing total length into material calculator; flat recalc fixed
